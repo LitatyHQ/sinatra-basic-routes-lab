7365a234cd26032790117fda9450f20c390e0fee
LitatyHQ <litatyHQ@gmail.com>
2021-01-14 21:36:26 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Litaty"
+  end
+
+  get '/hometown' do
+    "My hometown is Winston Salem"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is No Games"
+  end
+    
 end
